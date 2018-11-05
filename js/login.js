332732ece6ec5517e1b0bdb28a4a279b2ed26fb4
babybreath <jai2012@163.com>
2018-11-05 19:05:32 +0800

    use fetch API

--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
 // 登陆接口URl
-var LOGIN_URL = '/login.do';
+var LOGIN_URL = 'http://127.0.0.1/user/login';
 
 // DOM元素
 var loginBtn = document.getElementById('loginBtn');
@@ -10,47 +10,61 @@ var warnText = document.getElementById('warnText');
 
 // 注册登录按钮事件
 loginBtn.addEventListener('click', clickLoginBtn, false);
-// 注册密码眼睛鼠标移入事件
+// 密码眼睛鼠标移入事件
 eyePrompt.addEventListener('mouseover', mouseoverEyePrompt, false);
-// 注册密码眼睛鼠标移出事件
+// 密码眼睛鼠标移出事件
 eyePrompt.addEventListener('mouseout', mouseoutEyePrompt, false);
+// 密码眼睛鼠标点击事件
+eyePrompt.addEventListener('click', mouseoutEyeClick, false);
 
 // 点击登陆
-function clickLoginBtn(){
-	// 重复点击无效
-	if(loginBtn.className === 'loading') return;
-	
-	var usernameValue = username.value;
-	var passwordValue = password.value;
-	// 参数非空验证
-	if(!usernameValue || !passwordValue) return;
-	
-	loginBtn.className = 'loading';
-	reqwest({
-		url: LOGIN_URL,
-		method: 'post',
-		data: { username: usernameValue, password: passwordValue },
-		success: function (res) {
-			loginBtn.className = 'loginBtn';
-			if(res.retcode !== 0){
-			  	//warnText.innerHTML = res.msg;
-			}else{
-			  	//location.href = ...
-			}
-	    },
-	    error: function (e){
-	    	warnText.innerHTML = e.statusText;
-	    	loginBtn.className = 'loginBtn';
-	    }
-	});
+function clickLoginBtn() {
+  // 重复点击无效
+  if (loginBtn.className === 'loading') return;
+
+  var usernameValue = username.value;
+  var passwordValue = password.value;
+  // 参数非空验证
+  if (!usernameValue || !passwordValue) return;
+
+  loginBtn.className = 'loading';
+  var body = { username: usernameValue, password: passwordValue }
+  fetch(LOGIN_URL, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: { 'Content-Type': 'application/json', },
+    credentials: 'include',
+  }).then(function (res) {
+    return res.json();
+  }).then(function (res) {
+    // 正常回调处理
+    loginBtn.className = 'loginBtn';
+
+  }).catch(function (err) {
+    // 异常处理
+    loginBtn.className = 'loginBtn';
+  })
 }
 
+var currentPasswordType = 'password';
 // 鼠标移入密码眼睛
-function mouseoverEyePrompt(){
-	password.type = 'text';
+function mouseoverEyePrompt() {
+  password.type = 'text';
 }
 
 // 鼠标移出密码眼睛
-function mouseoutEyePrompt(){
-	password.type = 'password';
+function mouseoutEyePrompt() {
+  password.type = 'password';
+}
+
+// 鼠标点击密码眼睛
+function mouseoutEyeClick() {
+  eyePrompt.removeEventListener('mouseover', mouseoverEyePrompt);
+  eyePrompt.removeEventListener('mouseout', mouseoutEyePrompt);
+  if(currentPasswordType === 'password'){
+    password.type = currentPasswordType = 'text';
+  }else{
+    password.type = currentPasswordType = 'password';
+  }
+  password.focus();
 }
